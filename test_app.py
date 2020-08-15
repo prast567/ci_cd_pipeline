db22c213b6812f61453ac9724298edcaf3331a32
Prashant Kumar <pk611886@gmail.com>
2020-08-15 23:41:07 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,9 +5,3 @@ def test_app_up():
     response = requests.get(url)
     assert response.status_code == 200
     
-def test_predict():
-    data = {"CHAS":{"0":0},"RM":{"0":6.575},"TAX":{"0":296.0},"PTRATIO":{"0":15.3},"B":{"0":396.9},"LSTAT":{"0":4.98}}
-    url = 'https://flask-pred-app.azurewebsites.net:443/predict'
-    response = requests.post(url, data)
-    assert response.status_code == 200
-    
