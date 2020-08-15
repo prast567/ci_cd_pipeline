e3ae3b33dc6e8faa93b1fee2f34fd9ea2acb8923
Prashant Kumar <pk611886@gmail.com>
2020-08-15 21:50:51 +0530

    Update make_prediction.sh

--- make_prediction.sh
+++ make_prediction.sh
@@ -4,4 +4,25 @@ PORT=443
 echo "Port: $PORT"
 
 # POST method predict
-curl -X GET https://flask-pred-app.azurewebsites.net/predict
+curl -d '{  
+   "CHAS":{  
+      "0":0
+   },
+   "RM":{  
+      "0":6.575
+   },
+   "TAX":{  
+      "0":296.0
+   },
+   "PTRATIO":{  
+      "0":15.3
+   },
+   "B":{  
+      "0":396.9
+   },
+   "LSTAT":{  
+      "0":4.98
+   }
+}'\
+     -H "Content-Type: application/json" \
+     -X POST http://localhost:$PORT/predict
