e72a1425c5a15d78263b4877e63c3cd291ee4960
Prashant Kumar <pk611886@gmail.com>
2020-08-15 18:08:07 +0530

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import json
+import requests
+def test_app_up():
+    url = 'https://flask-pred-app.azurewebsites.net/'
+    response = requests.get(url)
+    assert 200 == 200
+    
+def test_predict():
+    url = 'https://flask-pred-app.azurewebsites.net/'
+    response = requests.get(url)
+    assert 200 == 200
+    
