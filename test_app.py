576b38f7f9598c061462841b344d9f923429f0ab
Prashant Kumar <pk611886@gmail.com>
2020-08-15 22:14:30 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,11 +2,12 @@ import json
 #import requests
 def test_app_up():
     url = 'https://flask-pred-app.azurewebsites.net/'
-    #response = requests.get(url)
-    assert 200 == 200
+    response = requests.get(url)
+    assert response.status_code == 200
     
 def test_predict():
-    url = 'https://flask-pred-app.azurewebsites.net/'
-    #response = requests.get(url)
-    assert 200 == 200
+    data = {"CHAS":{"0":0},"RM":{"0":6.575},"TAX":{"0":296.0},"PTRATIO":{"0":15.3},"B":{"0":396.9},"LSTAT":{"0":4.98}}
+    url = 'https://flask-pred-app.azurewebsites.net:443/predict'
+    response = requests.post(url, data)
+    assert response.status_code == 200
     
